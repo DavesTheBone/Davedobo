b990a7b98f853682362d70e4aa53ab338f6b17b6
Daves The Bone <davesthebone@gmail.com>
2025-02-12 23:17:13 +0900

    u so dumb

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -77,15 +77,15 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
+		['FUCK YOU!!!', 0.2], //From 0% to 19%
+		['U SUCK!!!', 0.4], //From 20% to 39%
+		['U SO DUMB', 0.5], //From 40% to 49%
+		['Bruhhh', 0.6], //From 50% to 59%
+		['Nahhh', 0.69], //From 60% to 68%
 		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
+		['Wow', 0.8], //From 70% to 79%
+		['EPIC!!!', 0.9], //From 80% to 89%
+		['Excellent!!', 1], //From 90% to 99%
 		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 
@@ -1153,8 +1153,8 @@ class PlayState extends MusicBeatState
 		}
 
 		var tempScore:String;
-		if(!instakillOnMiss) tempScore = Language.getPhrase('score_text', 'Score: {1} | Misses: {2} | Rating: {3}', [songScore, songMisses, str]);
-		else tempScore = Language.getPhrase('score_text_instakill', 'Score: {1} | Rating: {2}', [songScore, str]);
+		if(!instakillOnMiss) tempScore = Language.getPhrase('score_text', 'Score: {1} | Fails: {2} | You are: {3}', [songScore, songMisses, str]);
+		else tempScore = Language.getPhrase('score_text_instakill', 'Score: {1} | You are: {2}', [songScore, str]);
 		scoreTxt.text = tempScore;
 	}
 
